ec06fb0995660f431a61f289b93a298485559bb7
Daroga Mishra <Daroga.Mishra@coxautoinc.com>
2025-11-25 13:10:21 -0500

    Camera calibration changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDECODE2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDECODE2.java
@@ -191,8 +191,7 @@ public class TeleOpDECODE2 extends LinearOpMode {
     private void initializeAprilTag() {
         aprilTag = new AprilTagProcessor.Builder()
                 .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-                .setLensIntrinsics(1156.544, 1156.544, 640.0, 360.0)
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.RADIANS)  // ftcPose is always in radians
                 .build();
 
         try {
