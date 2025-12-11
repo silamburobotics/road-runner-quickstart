d58ad7a657062d10cbe287662707edd374690043
Daroga Mishra <Daroga.Mishra@coxautoinc.com>
2025-12-10 21:10:29 -0500

    added Abs function in the condition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDECODEBlueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDECODEBlueFar.java
@@ -407,8 +407,8 @@ public class AutoDECODEBlueFar extends LinearOpMode {
         
         // Get current position and calculate target
         double currentPosition = indexor.getCurrentPosition();
-        
-        if (currentPosition>(IndexerPreviousPosition + INDEXOR_TICKS))
+
+        if (Math.abs(currentPosition)>Math.abs(IndexerPreviousPosition + INDEXOR_TICKS))
         {
             targetPosition = currentPosition+INDEXOR_TICKS*2-(currentPosition % INDEXOR_TICKS);
         }
